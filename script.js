3f4745f93f2ad16d2cd2c4f9feb896e7e8547795
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2024-06-24 15:29:23 +0300

    im linking the shop page to the sproducts page

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ if (close) {
 }
 
 
+
+
+//js  practice
+
+// var mainImg  = document.getElementById("mainImg");
+// var smallImg = document.getElementsByClassName("small-img");
+
+// smallImg[0].onclick = function(){
+//     mainImg.src = smallImg[0].src;
+// }
+
+// smallImg[1].onclick = function(){
+//     mainImg.src = smallImg[1].src;
+// }
+// smallImg[2].onclick = function(){
+//     mainImg.src = smallImg[2].src;
+// }
